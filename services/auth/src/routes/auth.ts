e8403a9b780586c442904ae5f6af618ecdb6b06d
Karan Sapra <sapra7112@gmail.com>
2025-11-23 02:10:11 +0530

    Added updated dist

--- services/auth/src/routes/auth.ts
+++ services/auth/src/routes/auth.ts
@@ -4,6 +4,6 @@ import uploadFile from '../middleware/multer.js';
 
 const router = express.Router();
 
-router.post("/register",uploadFile, registerUser);
+router.post("/register", uploadFile, registerUser);
 
 export default router;
