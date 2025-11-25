094fb8209a7c69d643b0344d2536444e0822a51a
Karan Sapra <sapra7112@gmail.com>
2025-11-25 20:55:56 +0530

    Added skill API

--- services/user/dist/Routes/user.js
+++ services/user/dist/Routes/user.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAuth } from "../middleware/auth.js";
-import { getUserProfile, myProfile, updateProfilePic, updateResume, updateUserProfile } from "../controllers/user.js";
+import { addUserSkill, deleteSkillFromUser, getUserProfile, myProfile, updateProfilePic, updateResume, updateUserProfile } from "../controllers/user.js";
 import uploadFile from "../middleware/multer.js";
 const router = express.Router();
 router.get("/me", isAuth, myProfile);
@@ -8,4 +8,6 @@ router.get("/user-profile/:userId", isAuth, getUserProfile);
 router.put("/update-user", isAuth, updateUserProfile);
 router.put("/update-profile-pic", isAuth, uploadFile, updateProfilePic);
 router.put("/update-user-resume", isAuth, uploadFile, updateResume);
+router.post("/skill/add", isAuth, addUserSkill);
+router.delete("/skill/delete", isAuth, deleteSkillFromUser);
 export default router;
