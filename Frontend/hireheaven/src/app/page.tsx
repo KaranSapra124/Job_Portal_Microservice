438f191c5709117934622cd3c6668bba453670b9
Karan Sapra <sapra7112@gmail.com>
2025-12-07 15:37:40 +0530

    AI Service integrated on frontend

--- Frontend/hireheaven/src/app/page.tsx
+++ Frontend/hireheaven/src/app/page.tsx
@@ -1,13 +1,15 @@
 
 import CareerGuide from '@/components/Home/Career-guide'
 import Hero from '@/components/Home/hero'
+import ResumeAnalyzer from '@/components/Home/resume-analyzer'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      <Hero/>
-      <CareerGuide/>
+      <Hero />
+      <CareerGuide />
+      <ResumeAnalyzer />
     </div>
   )
 }
--- Frontend/hireheaven/src/type.ts
+++ Frontend/hireheaven/src/type.ts
@@ -24,4 +24,36 @@ export interface CareerGuideResponse {
     learningApproach: LearningApproach;
 }
 
+export interface ScoreBreakdown {
+    formatting: {
+        score: number;
+        feedback: string
+    };
+    keywords: {
+        score: number;
+        feedback: string
+    }
+    strucuture: {
+        score: number;
+        feedback: string
+    }
+    readability: {
+        score: number;
+        feedback: string
+    }
+}
+
+export interface Suggestion {
+    category: string;
+    issue: string;
+    recommendation: string;
+    priority: "high" | "medium" | "low"
+}
+export interface ResumeAnalysisResponse {
+    atsScore: number;
+    scoreBreakdown: ScoreBreakdown;
+    suggestions: Suggestion[];
+    strengths: string[];
+    summary: string;
+}
 export const utils_service = "http://localhost:5001"
